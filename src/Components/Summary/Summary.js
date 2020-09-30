da9eeea4a51e79361ccb7341d345e02ba8d0f3c6
Evisari Riyanto <evisari111012@gmail.com>
2020-09-30 15:03:01 -0700

    total of 6 components

--- src/Components/Summary/Summary.js
+++ src/Components/Summary/Summary.js
@@ -1,34 +1,13 @@
-import React from 'react'; 
-import Total from './Total';
+import React from "react";
+import Total from "./Total";
+import Items from "./Items";
 
-export default function Summary({selected}) {
-    const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD'
-      });
-
-    const summary = Object.keys(selected).map((feature, idx) => {
-        const featureHash = feature + '-' + idx;
-        const selectedOption = selected[feature];
-  
-        return (
-          <div className='summary__option' key={featureHash}>
-            <div className='summary__option__label'>{feature} </div>
-            <div className='summary__option__value'>{selectedOption.name}</div>
-            <div className='summary__option__cost'>
-              {USCurrencyFormat.format(selectedOption.cost)}
-            </div>
-          </div>
-        );
-      });
-  
-
-
-      return (
-      <section className='main__summary'>
+export default function Summary({ selected }) {
+  return (
+    <section className="main__summary">
       <h2>Your cart</h2>
-      {summary}
-        <Total selected={selected}/>
+      <Items selected={selected} />
+      <Total selected={selected} />
     </section>
-      )
+  );
 }
